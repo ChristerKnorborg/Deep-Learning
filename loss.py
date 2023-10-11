e8e3cce298c8924863f2abeeae92efa5d27fb25c
Christer <au644764@uni.au.dk>
2023-10-11 21:03:48 +0200

    loss yikes

--- loss.py
+++ loss.py
@@ -45,19 +45,58 @@ class YOLOLoss(nn.Module):
 
 
         # Determine which bounding box is responsible for the prediction
+        pred_bboxes1 = pred_boxes[..., 0, 1:5] # Extract the coordinates from the first bounding boxes
+        pred_bboxes2 = pred_boxes[..., 1, 1:5] # Extract the coordinates from the second bounding boxes
+        target_bboxes1 = target_boxes[..., 0, 1:5] # Extract the coordinates from the first bounding boxes
+        target_bboxes2 = target_boxes[..., 1, 1:5] # Extract the coordinates from the second bounding boxes
 
-        
-        iou1 = IoU(pred_boxes[..., 0, 1:5], target_boxes[..., 0, 1:5]) 
-        iou2 = IoU(pred_boxes[..., 1, 1:5], target_boxes[..., 1, 1:5])
 
-        if iou1 > iou2:
-            responsible_box_mask1 
+        iou1 = IoU(pred_bboxes1, target_bboxes1) 
+        iou2 = IoU(pred_bboxes2, target_bboxes2)
 
-        # Only compute loss for cells which contain objects
-        obj_mask = target_confidence > 0 # 1 if object exists in cell, 0 otherwise
-        pred_coords = pred_boxes[obj_mask, ..., 0] # get the 4 values of the tensor (x, y, w, h)
-        target_coords = target_boxes[obj_mask, ..., 0] # get the 4 values of the tensor (x, y, w, h)
-        coord_loss = self.lambda_coord * self.mse(pred_coords, target_coords) # MSE loss
+        responsible_box_mask1 = iou1 > iou2 # Mask of shape (batch_size, S, S, 1) where each entry is 1 if the first bounding box is responsible for the prediction, 0 otherwise
+        responsible_box_mask2 = ~responsible_box_mask1 
+
+        # Compute coordinate loss for bounding box 1
+        obj_mask1 = (target_confidence1 > 0) & responsible_box_mask1
+        pred_coords1 = pred_boxes[obj_mask1, ..., 0, 1:5]
+        target_coords1 = target_boxes[obj_mask1, ..., 0, 1:5]
+
+        # Separate x, y, w, h
+        pred_x1, pred_y1, pred_w1, pred_h1 = torch.split(pred_coords1, 1, dim=-1)
+        target_x1, target_y1, target_w1, target_h1 = torch.split(target_coords1, 1, dim=-1)
+
+        # Compute losses
+        loss_x1 = self.mse(pred_x1, target_x1)
+        loss_y1 = self.mse(pred_y1, target_y1)
+        loss_w1 = self.mse(torch.sqrt(pred_w1), torch.sqrt(target_w1))
+        loss_h1 = self.mse(torch.sqrt(pred_h1), torch.sqrt(target_h1))
+
+        # Combine the losses
+        coord_loss1 = loss_x1 + loss_y1 + loss_w1 + loss_h1
+
+        # Compute coordinate loss for bounding box 2
+        obj_mask2 = (target_confidence2 > 0) & responsible_box_mask2
+        pred_coords2 = pred_boxes[obj_mask2, ..., 1, 1:5]
+        target_coords2 = target_boxes[obj_mask2, ..., 1, 1:5]
+
+        # Separate x, y, w, h
+        pred_x2, pred_y2, pred_w2, pred_h2 = torch.split(pred_coords2, 1, dim=-1)
+        target_x2, target_y2, target_w2, target_h2 = torch.split(target_coords2, 1, dim=-1)
+
+        # Compute losses
+        loss_x2 = self.mse(pred_x2, target_x2)
+        loss_y2 = self.mse(pred_y2, target_y2)
+        loss_w2 = self.mse(torch.sqrt(pred_w2), torch.sqrt(target_w2))
+        loss_h2 = self.mse(torch.sqrt(pred_h2), torch.sqrt(target_h2))
+
+        # Combine the losses
+        coord_loss2 = loss_x2 + loss_y2 + loss_w2 + loss_h2
+
+        # Sum up the losses and weight with lambda_coord
+        total_coord_loss = self.lambda_coord * (coord_loss1 + coord_loss2)
+
+
 
 
         # ======================= #
@@ -95,9 +134,47 @@ class YOLOLoss(nn.Module):
 
 
 def IoU(box1, box2):
-    # Implement the IoU calculation between box1 and box2
-    pass
-    return
+    # Convert the (x, y, w, h) box format to (xmin, ymin, xmax, ymax)
+    box1 = [box1[..., 0] - box1[..., 2] / 2, box1[..., 1] - box1[..., 3] / 2,
+            box1[..., 0] + box1[..., 2] / 2, box1[..., 1] + box1[..., 3] / 2]
+    box2 = [box2[..., 0] - box2[..., 2] / 2, box2[..., 1] - box2[..., 3] / 2,
+            box2[..., 0] + box2[..., 2] / 2, box2[..., 1] + box2[..., 3] / 2]
+    
+    # Calculate the intersection coordinates
+    inter_x1 = torch.max(box1[0], box2[0])
+    inter_y1 = torch.max(box1[1], box2[1])
+    inter_x2 = torch.min(box1[2], box2[2])
+    inter_y2 = torch.min(box1[3], box2[3])
+
+    # Calculate intersection area
+    inter_area = torch.clamp(inter_x2 - inter_x1, min=0) * torch.clamp(inter_y2 - inter_y1, min=0)
+    
+    # Calculate union area
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    union_area = box1_area + box2_area - inter_area
+    
+    # Calculate IoU
+    iou = inter_area / union_area
+
+    return iou
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 label = torch.Tensor(([
